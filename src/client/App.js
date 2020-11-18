59027619bf2229eb1897a4aebc8f168d051144e8
tengfone <phangtf@gmail.com>
2020-11-18 16:44:11 +0800

    fixed visualization

--- src/client/App.js
+++ src/client/App.js
@@ -12,15 +12,16 @@ export default class App extends Component {
         <body>
           <FileUpload />
           <section id="section1">
-          <midi-visualizer
+          <midi-player
+            src="https://cdn.jsdelivr.net/gh/cifkao/html-midi-player@2b12128/twinkle_twinkle.mid"
+            sound-font="https://storage.googleapis.com/magentadata/js/soundfonts/sgm_plus" visualizer="#section1 midi-visualizer">
+          </midi-player>
+                    <midi-visualizer
             type="staff"
             src="https://cdn.jsdelivr.net/gh/cifkao/html-midi-player@2b12128/twinkle_twinkle.mid">
           </midi-visualizer>
-          <midi-player src="https://cdn.jsdelivr.net/gh/cifkao/html-midi-player@2b12128/twinkle_twinkle.mid"></midi-player>
-          {/* <midi-player
-            src="https://cdn.jsdelivr.net/gh/cifkao/html-midi-player@2b12128/twinkle_twinkle.mid"
-            sound-font visualizer="#section1 midi-visualizer">
-          </midi-player> */}
+          {/* <midi-player src="https://cdn.jsdelivr.net/gh/cifkao/html-midi-player@2b12128/twinkle_twinkle.mid"></midi-player> */}
+
           </section>
         </body>
       </div>
